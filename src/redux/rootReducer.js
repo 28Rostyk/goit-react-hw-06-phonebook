5a810bfc489e88dac2e9ef48de6f61e47e1e9ddc
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-02-23 19:26:42 +0200

    Update rootReducer.js

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,9 +3,11 @@ import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import contactsReducer from './contacts/contacts-slice';
+import filterReducer from './filter/filterSlice';
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
+  filter: filterReducer,
 });
 
 const persistConfig = {
